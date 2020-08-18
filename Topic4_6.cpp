6cd5e6a08f3be789b745482e66b24909466fe21b
Rollcorn <57489789+Rollcorn@users.noreply.github.com>
2020-08-19 02:57:04 +0600

    Глава 4 Задача 6
    Программа выполняет поставленные задачи. Кроме вывода минимального и максимального значения после окончания ввода.

--- Topic4_6.cpp
+++ Topic4_6.cpp
@@ -6,26 +6,27 @@
 //и определяет наименьшее и наибольшее значение среди всех ранее
 //введенных значений
 
-void enter_numbers(std::vector<double> vr){
+void enter_numbers(std::vector<double>* vr){
     double max_elem;
     double min_elem;
     std::cout<<"Enter some numbers: ";
-    for(double temp; std::cin >>  temp;){
+    for(double temp; std::cin >>  temp; ){
+        vr->push_back(temp);
+        if(temp == '|') break;
         std::cout << temp << std::endl;
-        if(!vr.empty()){
+        if(vr->size() != 1){
             if(max_elem < temp){
                max_elem = temp;
-               std::cout << "Наибольшее число: " << max_elem <<
+               std::cout << "Current MAX number is:  " << max_elem <<
                                                      std::endl;
-            }
+            };
             if(min_elem > temp){
                 min_elem = temp;
-                std::cout << "Наименьшее число: " << min_elem <<
+                std::cout << "Current MIN number is: " << min_elem <<
                                                      std::endl;
         }else{ max_elem = temp;
                min_elem = temp;
         };
-        vr.push_back(temp);
         };
     }
 }
@@ -33,7 +34,7 @@ void enter_numbers(std::vector<double> vr){
 int main()
 {
     std::vector<double> vec;
-    enter_numbers(vec);
+    enter_numbers(&vec);
         
     std::cout << "The list of numbers: ";
     for(int i=0; i<vec.size();i++){
