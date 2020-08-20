b1b9e4855006756513d349f9908d58e56cca5899
Rollcorn <57489789+Rollcorn@users.noreply.github.com>
2020-08-20 21:19:07 +0600

    Add files via upload
    Программа выполняет задачу - угадывает загаданное число. За исключением того что некоторые ситуации занимают более 7 итераций.
    Возможно стоит сделать отсеивание по четности числа, а также попробовать использовать аллокацию вектора и его внутренние итераторы для выполнения задачи.

--- Exercise4_4.cpp
+++ Exercise4_4.cpp
@@ -10,23 +10,21 @@
 void guess_number(uint16_t lower_bound, uint16_t upper_bound){
     std::string guess;
     std::string answr;
-    std::cout << "Your number is less than " << upper_bound/2 << "? \n";
+    std::cout << "Your number is less than " << 
+                (upper_bound+lower_bound)/2 << "? \n";
     std::cin >> answr;
     if(answr == "y"){ //if get yes - number less than upper_bound/2;
-        upper_bound = upper_bound/2;
-        std::cout << "You guess " << (upper_bound/2)+lower_bound 
-                                  << " ?\n";
-        std::cin >> guess;
+        upper_bound = (upper_bound + lower_bound)/2;
 
     }else if(answr == "n"){
-        lower_bound = upper_bound/2;
-        std::cout << "You guess " << (upper_bound/2)+lower_bound 
-                                  << " ?\n";
-        std::cin >> guess;
-   
+        lower_bound = (upper_bound + lower_bound)/2;
     };
-        if(guess == "yes" || guess == "y" || guess == "Yes") 
-        std::cout <<  upper_bound/2;
+    std::cout << "You guess " << (upper_bound+lower_bound)/2 
+                                  << " ?\n";
+    std::cin >> guess;
+ 
+    if(guess == "yes" || guess == "y" || guess == "Yes") 
+        std::cout <<  (upper_bound + lower_bound)/2 << "\n";
     else if(guess == "n" || guess == "no" || guess == "No") {
         guess_number(lower_bound, upper_bound);};
 };     
