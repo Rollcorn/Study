6704728802db6e7984cf61ee3446d3f5d40de5d6
Rollcorn <57489789+Rollcorn@users.noreply.github.com>
2020-08-20 20:07:16 +0600

    Глава 4 Упражнение 4
    Программа угадывает число. Пользователь загадывает число от 1 до 100, а программа задает вопросы, чтобы выяснить какое число он задумал.
    Не доделал, угадывает число только в меньшую сторону. Не возвращает большую половину диапазона.

---
+++ Exercise4_4.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+//Глава 4 Упражнение 4: Программа угадывает число. Пользователь загадывает
+//число от 1 до 100, а программа задает вопросы, чтобы выяснить какое 
+//число он задумал.
+
+void guess_number(uint16_t lower_bound, uint16_t upper_bound){
+    std::string guess;
+    std::string answr;
+    std::cout << "Your number is less than " << upper_bound/2 << "? \n";
+    std::cin >> answr;
+    if(answr == "y"){ //if get yes - number less than upper_bound/2;
+        upper_bound = upper_bound/2;
+        std::cout << "You guess " << (upper_bound/2)+lower_bound 
+                                  << " ?\n";
+        std::cin >> guess;
+
+    }else if(answr == "n"){
+        lower_bound = upper_bound/2;
+        std::cout << "You guess " << (upper_bound/2)+lower_bound 
+                                  << " ?\n";
+        std::cin >> guess;
+   
+    };
+        if(guess == "yes" || guess == "y" || guess == "Yes") 
+        std::cout <<  upper_bound/2;
+    else if(guess == "n" || guess == "no" || guess == "No") {
+        guess_number(lower_bound, upper_bound);};
+};     
+
+
+
+int main(){
+    std::string answr;
+    uint16_t upper_bound;
+    uint16_t lower_bound;
+    std::cout << "Enter guessing range: \n";
+    std::cin >> lower_bound >> upper_bound;
+    std::cout << "Guess the number from " << lower_bound << 
+                  " to " << upper_bound  << std::endl;
+    
+    guess_number(lower_bound, upper_bound);    
+
+
+return 0;
+}
+
