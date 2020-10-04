5c2f20bdecd095b2b8ce467f59193f986498adf1
rollcorn <lazydaimon@gmail.com>
2020-10-05 01:51:17 +0300

    Программа считывает размеры матрицы, элементы и выводит её

--- Algorithms/matrix_prod.cpp
+++ Algorithms/matrix_prod.cpp
@@ -7,35 +7,36 @@ const static int MAX_COLUMNS = 100;
 const static int MAX_ROWS = 100;
 using namespace std;
 
-void matrix_fill(int matrix[MAX_COLUMNS][MAX_ROWS]){
+void matrix_fill(int matrix[MAX_ROWS][MAX_COLUMNS]){
     int col_numb;
     int row_numb;
     int number;
     cout << "Please enter numbers of columns: " << endl;
-    cin >> col_numb;
-    cout << "Please enter numbers of rows: "  << endl;
     cin >> row_numb;
+    cout << "Please enter numbers of rows: "  << endl;
+    cin >> col_numb;
     for(int j = 0; j != row_numb ; j++){
         for(int i = 0; i != col_numb; i++){
             cout << "Enter some number: " << endl;
             cin >> number;
-            matrix[i][j] = number;
+            matrix[j][i] = number;
         }
     }
-    for(int j; j != row_numb ; j++){
-        for(int i; i != col_numb; i++){
-            cout << matrix[i][j] << endl; 
+    for(int j = 0; j != row_numb ; j++){
+        for(int i = 0; i != col_numb; i++){
+            cout << matrix[j][i] << "\t"; 
         }
+        cout << "\n";
     }
 
 }
 
 int main(){
     int matrix_1[MAX_COLUMNS][MAX_ROWS];
-    int matrix_2[MAX_COLUMNS][MAX_ROWS];
+//    int matrix_2[MAX_COLUMNS][MAX_ROWS];
         
     matrix_fill(matrix_1);
-    matrix_fill(matrix_2);
+//    matrix_fill(matrix_2);
 
 return 0;
 }
