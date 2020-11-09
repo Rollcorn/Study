48f96cc54e3e8464da4bb182f6b4ab2d93963128
rollcorn <lazydaimon@gmail.com>
2020-11-09 15:04:24 +0300

    Simple Smart pointer realization

---
+++ Multithreading/Smart_pointer.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+template <class Type>
+class SmartPointer {
+private:
+    Type* pointer;
+public:
+    SmartPointer(Type* p):pointer(p) {};
+    operator Type*() {return pointer; };
+    Type* operator->() {
+        if(!pointer){
+            pointer = new Type();
+            std::cerr << "Bad pointer!" << std::endl;
+        }
+        return pointer; 
+    };
+    std::ptrdiff_t operator-(SmartPointer<Type> p){
+        return pointer - p;
+    }
+    std::ptrdiff_t operator-(void* p){
+        return pointer - p;
+    }
+};
+
+class Foo {
+private:
+    int a, b;
+public:
+    Foo(): a(0), b(0) {};
+    Foo(int a, int b): a(a), b(b) {};
+    int Sum() {return a+b; }
+};
+
+int main(int argc, char **argv) {
+//    SmartPointer<Foo> sp(new Foo(2,2));
+    SmartPointer<Foo> sp(NULL);
+    std::cout << sp->Sum() << std::endl;
+
+    return 0;
+}
+
+
+
+
+
