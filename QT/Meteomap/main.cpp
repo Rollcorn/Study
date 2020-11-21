fede1da76a37d91fb188682b6faee4d861b3432d
rollcorn <lazydaimon@gmail.com>
2020-11-22 02:22:43 +0300

    Программа отображает города на сцене, без коллизий. Реализовать вывод названий городов и их приоритет

--- QT/Meteomap/main.cpp
+++ QT/Meteomap/main.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 #include <QWidget>
 
+
 #include "meteo.h"
 #include "mongoConnect.h"
-
-//const int WINDOW_SIZE = 600;
-
 
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
-    QGraphicsScene scene(QRectF(0, 0, 500, 400));
+    QGraphicsScene scene(QRectF(0, 0, 1080, 540));
     QGraphicsView view(&scene);
 
-    view.setFixedSize(QSize(600, 500));
+    view.setFixedSize(QSize(1100, 550));
 
     mongocxx::instance  inst{};
     mongocxx::uri       myURI("mongodb://localhost:27017");
@@ -36,23 +34,43 @@ int main(int argc, char** argv)
              bsoncxx::document::element myPriority = (*iter)["priority"];
              bsoncxx::document::element myMinLvl = (*iter)["min_level"];
              bsoncxx::document::element myMaxLvl = (*iter)["max_level"];
-
+//Получаю данные из БД
              std::string strName = myName.get_string().value.to_string();
-         //    double strLocation = double((myLocation.get_array().value[0]).get_double());
+             double x = double((myLocation.get_array().value[0]).get_double());
+             double y = double((myLocation.get_array().value[1]).get_double());
              int32_t strPriority = int32_t(myPriority.get_int32().value);
              int32_t strMinLvl = int32_t(myMinLvl.get_int32().value);
              int32_t strMaxLvl = int32_t(myMaxLvl.get_int32().value);
 
 
-
-             std::cout << "Имя: " << strName << " "
-                       << "Локация: " << double((myLocation.get_array().value[0]).get_double()) << " "
-                                      << double((myLocation.get_array().value[1]).get_double()) << " "
-                       << "Приоретет: " << strPriority << " "
-                       << "Минимальный: " << strMinLvl << " "
+             std::cout << "Имя: " << strName << "; "
+                       << "Локация: " << x << " : "
+                                      << y << "; "
+                       << "Приоретет: " << strPriority << "; "
+                       << "Минимальный: " << strMinLvl << "; "
                        << "Максимальный: " << strMaxLvl
                        << std::endl;
+//Создаю объект обозначающих город в данной точке.
+                QGraphicsRectItem* cityPoint = scene.addRect(QRectF(0, 0, 10, 10), QPen(Qt::black), QBrush(Qt::red));
+//Преобразую координаты карты к координатам сцены на экране.
+                double locX;
+                double locY;
+                if(x <= 0){
+                    x = qAbs(x);
+                    locX = (180 - x);
+                }else locX = 180 + x;
 
+                if(y <= 0){
+                    y = qAbs(y);
+                    locY = 90 + y;
+                }else locY = 90 - y;
+
+                cityPoint->setPos(3*locX, 3*locY);
+//Прячу эллементы в случае коллизии.
+                if(!cityPoint->collidingItems().isEmpty()){
+                    cityPoint->hide();
+                }
+             view.show();
 
             }
          } catch (const std::exception& xcp){
@@ -60,8 +78,6 @@ int main(int argc, char** argv)
          return EXIT_FAILURE;
      }
 
-    view.show();
     return app.exec();
-
 }
 
