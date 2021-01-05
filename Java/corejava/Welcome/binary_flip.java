a9a0a789b46d48bb5baa0b999451388fe2cebed3
rollcorn <lazydaimon@gmail.com>
2021-01-06 01:40:02 +0300

    My java projects

---
+++ Java/corejava/Welcome/binary_flip.java
@@ -0,0 +1,14 @@
+public class binary_flip{
+
+    public static void main(String[] args) {
+        System.out.print(flipBit(7,1));
+    }
+    
+    public static int flipBit(int value, int bitIndex) {
+        int value2 = 0;
+         
+
+
+        return  value2;
+    } 
+}   
