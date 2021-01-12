27dfebf4de7686d5f2deceb1cb6e1e3a2a9f3b67
rollcorn <lazydaimon@gmail.com>
2021-01-13 02:33:42 +0300

    Work with binary operations

--- Java/corejava/Welcome/binary_flip.java
+++ Java/corejava/Welcome/binary_flip.java
@@ -1,14 +1,10 @@
 public class binary_flip{
 
     public static void main(String[] args) {
-        System.out.print(flipBit(7,1));
+        System.out.print(flipBit(7,3));
     }
     
     public static int flipBit(int value, int bitIndex) {
-        int value2 = 0;
-         
-
-
-        return  value2;
+        return  value ^ (1 << bitIndex - 1);
     } 
 }   
