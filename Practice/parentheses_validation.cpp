08f026e71aa90ba759337b98c7efbea5dd43145a
rollcorn <lazydaimon@gmail.com>
2021-03-07 11:40:36 +0300

    Final build of function for parentheses validation check

--- Practice/parentheses_validation.cpp
+++ Practice/parentheses_validation.cpp
@@ -12,45 +12,80 @@
 
 #include <iostream>
 #include <stack>
-#include <vector>
+#include <string>
+
+void parentheses_valid(std::string seq){
+ 
+    std::stack<char> stack;
+  
+    for(auto i = seq.begin(); i != seq.end(); i++){
+        
+        if((*i == ')' or *i =='}' or *i == ']') and stack.empty()) stack.push(*i);
+        
+        switch(*i){
+        case(')'):
+            if(stack.top() == '(') {stack.pop();}
+            else {stack.push(*i);}
+            break;
+        case('}'):
+            if(stack.top() == '{') stack.pop();
+            else {stack.push(*i);}
+            break;
+        case(']'):
+            if(stack.top() == '[') stack.pop();
+            else {stack.push(*i);}
+            break;
+        default:
+            if(*i == '(' || *i == '{' || *i == '[') {
+            stack.push(*i);
+            }
+        }
+    }
+   
+    std::cout << "Resul is: " << std::endl;
+    
+    for (auto i = seq.begin(); i != seq.end(); i++) std::cout << *i << " ";
+    std::cout << std::endl;
+    
+    if (stack.empty()) std::cout << "It is RIGHT sequence!" << std::endl;
+    else std::cout << "It is BAD sequence!" << std::endl;
+}
+
 
 int main(){
     
-    std::stack<char> stack;
-    std::vector<char> seq;
-    char el;
+    std::string seq;
     
-    std::cout << "Enter your sequence (press R to finish enter): " << std::endl;
-    while (std::cin >>  el && el != 'R')
-        seq.push_back(el); 
-    std::cout << "Resul is: " << std::endl;
-    for(int i = 0; i != seq.size(); i++){
-            switch(seq[i]){
-            case(')'):
-                if(stack.top() == '(' && !stack.empty()) stack.pop();
-                
-                break;
-            case('}'):
-                if(stack.top() == '{') stack.pop();
-                break;
-            case(']'):
-                if(stack.top() == '[') stack.pop();
-                break;
-            default:
-                if(seq[i] == '(' || seq[i] == '{' || seq[i] == '[') {
-                    stack.push(seq[i]);
-                } else {
-                    std::cout << "Bad enter!" << std::endl;
-               }
-            }
-            
-    }
-    for(int i = 0; i != seq.size(); i++) std::cout << seq[i] << " ";
-    std::cout << std::endl;
-    if(stack.empty()) std::cout << "Is right sequence!" << std::endl;
-    else std::cout << "Is BAD sequence!" << std::endl;
+    std::cout << "Enter your sequence: " << std::endl;
+    std::getline(std::cin, seq);
+    parentheses_valid(seq); 
     
 
+    //Some tests:
+    std::cout << "Test 1-f" << '\n';
+    std::string test1 = "{)";
+    parentheses_valid(test1);
+    
+    std::cout << "Test 2-f" << '\n';
+    std::string test2 = ")";
+    parentheses_valid(test2);
+    
+    std::cout << "Test 3-f" << '\n';
+    std::string test3 = "(]]]]";
+    parentheses_valid(test3);
+    
+    std::cout << "Test 4-t" << '\n';
+    std::string test4 = "(){}[]";
+    parentheses_valid(test4);
+    
+    std::cout << "Test 5-f" << '\n';
+    std::string test5 = "}}}}";
+    parentheses_valid(test5);
+    
+    std::cout << "Test 6-f" << '\n';
+    std::string test6 = "[[[[";
+    parentheses_valid(test6);
+
+ 
     return 0;
-
 }
